4f8b2d5ca5158168cdd198b758e017d4dddec092
cheesequake <dev.cheesequake@gmail.com>
2024-01-23 10:06:30 +0530

    FormSubmission

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,16 @@ import Form from './components/Form';
 import SportIndividual from './components/SportIndividual';
 import SportTeam from './components/SportTeam';
 
+
 function App() {
   const [mode, setMode] = useState("individual");
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState(0);
+  const [gender, setGender] = useState("");
+  const [collegeName, setCollegeName] = useState("");
+  const [resState, setResState] = useState("");
+  const [city, setCity] = useState("");
   return (
     <div className="min-h-screen min-w-full bg-[url('./assets/universalBg.jpg')] bg-no-repeat bg-cover bg-center flex justify-evenly items-center flex-col">
       <div className="flex justify-evenly items-center w-4/5">
@@ -15,17 +23,21 @@ function App() {
         <ModeSelector setMode = {setMode}/>
       </div>
       <div className="flex justify-around items-center h-96 w-4/5">
-        <Form />
+        <Form setFullName = {setFullName} setEmail = {setEmail} setPhoneNumber = {setPhoneNumber} setGender = {setGender} setCollegeName = {setCollegeName} setResState = {setResState} setCity = {setCity} />
         {mode=="individual" && <SportIndividual />}
         {mode=="team" && <SportTeam />}
       </div>
       <div className="flex justify-start items-center w-4/5">
         <div className="w-5/12 flex justify-center items-center">
-          <button className="border-white border-2 rounded-lg text-white text-2xl px-5 py-2 font-Tourney font-black">REGISTER</button>
+          <button className="border-white border-2 rounded-lg text-white text-2xl px-5 py-2 font-Tourney font-black" onClick={()=>Register(fullName, email, phoneNumber, gender, collegeName, resState, city)}>REGISTER</button>
         </div>
       </div>
     </div>
   )
 }
 
-export default App
+function Register(fullName, email, phoneNumber, gender, collegeName, resState, city){
+  console.log(fullName, email, phoneNumber, gender, collegeName, resState, city);
+}
+
+export default App;
