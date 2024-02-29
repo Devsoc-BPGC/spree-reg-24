1930b5449308d686a48be2e2a2dc3bab354f905d
arjit120604 <130384377+arjit120604@users.noreply.github.com>
2024-03-01 01:50:33 +0530

    navbar

--- src/Lapp.jsx
+++ src/Lapp.jsx
@@ -1,60 +1,86 @@
 import land from "./assets/landing_img.svg";
-import insta from "./assets/insta.png"
-import { Link } from 'react-router-dom'
+import insta from "./assets/insta.png";
+import { Link } from "react-router-dom";
+import NavBar from "./components/NavBar";
 import "./Lapp.css";
 
 function Lapp() {
   const styles = {
     backgroundImage: `url(${land})`,
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'center',
-    width: '100vw',
-    height: '100vh',
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+    width: "100vw",
+    height: "100vh",
   };
 
-  const buttonWidth = '130px'; // Set the width of the button
+  const buttonWidth = "130px"; // Set the width of the button
 
   const imageStyle = {
     width: 130,
-    height: 59*130/200, // Ensure the image has the same dimensions as the button
-    cursor: 'pointer',
+    height: (59 * 130) / 200, // Ensure the image has the same dimensions as the button
+    cursor: "pointer",
   };
 
   const handleClick = () => {
-    
-    window.location.href = 'https://www.instagram.com/';
+    window.location.href = "https://www.instagram.com/";
   };
 
   return (
-    <div style={styles} className='bgimg'>
-      <div className='lwright'>
-      <div className='regbtn'>
-        <Link to="/register">
-        <button style={{ 
-          fontSize: '15px', 
-          color:'white', 
-          border: '2.85px solid white', 
-          padding: '8px 13px', 
-          background: 'rgba(0, 0, 0, 1)', 
-          borderRadius: '999px', 
-          cursor: 'pointer',
-          width: buttonWidth, // Set the width of the button
-          marginRight:'-13px',
-          letterSpacing:'0.25em'
-        }}>REGISTER</button>
-        </Link>
-      </div> 
-      <div className='text' style={{ width: 200   , color:'#FEDF3E', fontSize:'9.7px', textAlign: 'center' }}> 
-        <p>Embrace the challenge, rally for your convictions, and let the intensity of competition ignite the passion within.</p>
-      </div>
-      <div className='follow'>
-       
-      <a href="https://www.instagram.com/bitsspree/?hl=en" target="_blank" rel="noopener noreferrer"> {/* Open the link in a new tab */}
-          <img src={insta} alt="Image" style={imageStyle} onClick={handleClick} /> {/* Set the image src, alt text, and style */}
-        </a>
-        
-      </div>  
+    <div style={styles} className="bgimg">
+      <NavBar />
+      <div className="lwright">
+        <div className="regbtn">
+          <Link to="/register">
+            <button
+              style={{
+                fontSize: "15px",
+                color: "white",
+                border: "2.85px solid white",
+                padding: "8px 13px",
+                background: "rgba(0, 0, 0, 1)",
+                borderRadius: "999px",
+                cursor: "pointer",
+                width: buttonWidth, // Set the width of the button
+                marginRight: "-13px",
+                letterSpacing: "0.25em",
+              }}
+            >
+              REGISTER
+            </button>
+          </Link>
+        </div>
+        <div
+          className="text"
+          style={{
+            width: 200,
+            color: "#FEDF3E",
+            fontSize: "9.7px",
+            textAlign: "center",
+          }}
+        >
+          <p>
+            Embrace the challenge, rally for your convictions, and let the
+            intensity of competition ignite the passion within.
+          </p>
+        </div>
+        <div className="follow">
+          <a
+            href="https://www.instagram.com/bitsspree/?hl=en"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            {/* Open the link in a new tab */}
+            <img
+              src={insta}
+              alt="Image"
+              style={imageStyle}
+              onClick={handleClick}
+            />{" "}
+            {/* Set the image src, alt text, and style */}
+          </a>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,46 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
+  darkMode: ["class"],
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,jsx}",
+    "./components/**/*.{js,jsx}",
+    "./app/**/*.{js,jsx}",
+    "./src/**/*.{js,jsx}",
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       fontFamily: {
         Tourney: ["Tourney", "sans-serif"],
-       },
-       colors: {
-        bblue: '#089EA8',
-        rred: '#BF3535',
-        ggrey: '#E5E5E5'
-       },
+        Alata: ["Alata"],
+      },
+      colors: {
+        bblue: "#089EA8",
+        rred: "#BF3535",
+        ggrey: "#E5E5E5",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
-  plugins: [],
-}
-
+  plugins: [require("tailwindcss-animate")],
+};
