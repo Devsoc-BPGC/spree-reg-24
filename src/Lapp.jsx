c121a38e3379cdf8d801f6ba7ec4f250b28515cb
Manamrit Singh <amrit20eclectic@gmail.com>
2024-02-29 22:34:56 +0530

    Landing Page and Routing

---
+++ src/Lapp.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import land from "C:/Users/longi/WebD/spree-reg-24/src/assets/landing_img.svg";
+import insta from "./assets/insta.png"
+import { Link } from 'react-router-dom'
+import "./Lapp.css";
+
+function Lapp() {
+  const styles = {
+    backgroundImage: `url(${land})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    width: '100vw',
+    height: '100vh',
+  };
+
+  const buttonWidth = '130px'; // Set the width of the button
+
+  const imageStyle = {
+    width: 130,
+    height: 59*130/200, // Ensure the image has the same dimensions as the button
+    cursor: 'pointer',
+  };
+
+  const handleClick = () => {
+    
+    window.location.href = 'https://www.instagram.com/';
+  };
+
+  return (
+    <div style={styles} className='bgimg'>
+      <div className='lwright'>
+      <div className='regbtn'>
+        <Link to="/register">
+        <button style={{ 
+          fontSize: '20px', 
+          color:'white', 
+          border: '2.85px solid white', 
+          padding: '4px 0px', 
+          background: 'rgba(0, 0, 0, 1)', 
+          borderRadius: '999px', 
+          cursor: 'pointer',
+          width: buttonWidth, // Set the width of the button
+          marginRight:'-13px',
+        }}>REGISTER</button>
+        </Link>
+      </div> 
+      <div className='text' style={{ width: 200   , color:'#FEDF3E', fontSize:'9.7px', textAlign: 'center' }}> 
+        <p>Embrace the challenge, rally for your convictions, and let the intensity of competition ignite the passion within.</p>
+      </div>
+      <div className='follow'>
+       
+      <a href="https://example.com" target="_blank" rel="noopener noreferrer"> {/* Open the link in a new tab */}
+          <img src={insta} alt="Image" style={imageStyle} onClick={handleClick} /> {/* Set the image src, alt text, and style */}
+        </a>
+        
+      </div>  
+      </div>
+    </div>
+  );
+}
+
+export default Lapp;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Rapp from './Rapp.jsx'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Lapp from './Lapp.jsx'
 import App from './App.jsx'
 import './index.css'
 
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+   <App/>
+  </React.StrictMode>
+);
